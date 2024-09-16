ec3c32c8bc0211959d5dfe9c7e724a701276c97d
abylaikossay <abylai.kossay@gmail.com>
2024-09-16 20:41:33 +0500

    feat: added header and slider

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from "./components/Header";
+import Slider from "./components/Slider";
+
+function App() {
+    return (
+        <div className="App">
+            <Header></Header>
+            <Slider></Slider>
+        </div>
+    );
+}
+
+export default App;
